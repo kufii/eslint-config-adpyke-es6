18c791bb244c2cc9b740d8736ddb3cf0dcce1096
Adrien Pyke <adpyke@gmail.com>
2018-03-05 13:43:34 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+	'extends': 'eslint:recommended',
+	'rules': {
+		'no-console': 'off',
+		'semi': 2,
+		'semi-style': 2,
+		'no-var': 2,
+		'no-whitespace-before-property': 2,
+		'no-eval': 2,
+		'no-implied-eval': 2,
+		'eqeqeq': 2,
+		'no-implicit-globals': 2,
+		'no-invalid-this': 2,
+		'no-labels': 2,
+		'no-with': 2,
+		'strict': 2,
+		'no-duplicate-imports': 2,
+		'prefer-rest-params': 2,
+		'rest-spread-spacing': 2,
+		'no-lone-blocks': 1,
+		'dot-notation': 1,
+		'prefer-arrow-callback': 1,
+		'prefer-spread': 1,
+		'no-unneeded-ternary': 1,
+		'no-useless-call': 1,
+		'no-useless-concat': 1,
+		'no-useless-escape': 1,
+		'no-useless-return': 1,
+		'no-useless-computed-key': 1,
+		'no-useless-constructor': 1,
+		'no-useless-rename': 1,
+		'object-shorthand': 1,
+		'arrow-parens': [1, 'as-needed'],
+		'yoda': 1,
+		'no-trailing-spaces': 1,
+		'no-multi-spaces': 1,
+		'no-floating-decimal': 1,
+		'quotes': [1, 'single'],
+		'indent': [1, 'tab'],
+		'no-mixed-spaces-and-tabs': [1, 'smart-tabs'],
+		'arrow-spacing': 1,
+		'generator-star-spacing': 1,
+		'template-curly-spacing': 1
+	},
+	'env': {
+		'es6': true,
+		'browser': true
+	}
+};
