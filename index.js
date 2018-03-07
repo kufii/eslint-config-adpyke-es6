0818e31d86ddf6b53395634fc4268b6c65fadb7e
Adrien Pyke <adpyke@gmail.com>
2018-03-07 15:25:55 -0500

    actually fixed flip

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ module.exports = {
 		'keyword-spacing': 1,
 		'no-lonely-if': 1,
 		'no-mixed-operators': 1,
-		'no-multiple-empty-lines': [1, { max: 2 }],
+		'no-multiple-empty-lines': [1, { max: 2, maxEOF: 0, maxBOF: 0 }],
 		'nonblock-statement-body-position': 1,
 		'object-curly-spacing': [1, 'always'],
 		'operator-linebreak': 1,
